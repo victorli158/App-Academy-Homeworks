3e5fd28fb8350638e743b315b18e2849ba3de4b7
Victor Li <victor.li158@gmail.com>
2017-05-10 08:27:38 -0700

    Complete W7D3

---
+++ W7D3/skeleton/giphy_search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import { fetchSearchGiphys } from './util/api_util';
+import { receiveSearchGiphys } from './actions/giphy_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const store = configureStore();
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
+});
