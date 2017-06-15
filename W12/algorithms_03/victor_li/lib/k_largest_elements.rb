c767aef247e0003da3fa11bc6bfff2208cf89a4b
Victor Li <victor.li158@gmail.com>
2017-06-15 11:08:42 -0700

    Finish algorithms 03 project

--- W12/algorithms_03/victor_li/lib/k_largest_elements.rb
+++ W12/algorithms_03/victor_li/lib/k_largest_elements.rb
@@ -1,5 +1,17 @@
 require_relative 'heap'
+require_relative 'heap_sort'
 
 def k_largest_elements(array, k)
-
+  heap = BinaryMinHeap.new()
+  array.each do |el|
+    heap.store.push(el)
+  end
+  heap.store.heap_sort!
+  largest = []
+  counter = -1
+  until largest.length == k
+    largest << heap.store[counter]
+    counter -= 1
+  end
+  largest
 end
